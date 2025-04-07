b9979f6513106ba8cf9b000e06464ff3931d5ad3
Gustavo Henrique Nicolete Pinheiro <ghnpinheiro@gmail.com>
2025-04-07 15:43:17 -0300

    Testes no código

--- git.js
+++ git.js
@@ -3,4 +3,5 @@ console.log(`Lista de todas as comidas: ${comidas}`);
 console.log(`Primeira Comida: ${comidas[0]}`);
 comidas.sort();
 console.log(`Comidas ordenadas: ${comidas}`);
+console.log("FIM =)");
 
