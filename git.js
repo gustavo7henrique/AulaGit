a4a61c655b7814f70dd09420899f6a212b61830e
Gustavo Henrique Nicolete Pinheiro <ghnpinheiro@gmail.com>
2025-04-07 15:19:59 -0300

    Incluindo ordenação da lista

--- git.js
+++ git.js
@@ -1,4 +1,6 @@
 let comidas = ["Lasanha", "Pizza", "Sushi", "Hamburguer"];
 console.log(`Lista de todas as comidas: ${comidas}`);
 console.log(`Primeira Comida: ${comidas[0]}`);
+comidas.sort();
+console.log(`Comidas ordenadas: ${comidas}`);
 
